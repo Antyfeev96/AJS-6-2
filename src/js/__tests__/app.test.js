5442c572b2767fd8abca690ed2e6bb680734d9b7
Dmitri Antyfeev <60137521+Antyfeev96@users.noreply.github.com>
2021-01-05 16:42:48 +0300

    Job done 2.0

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,93 +1,44 @@
-/* eslint-disable object-curly-newline */
-/* eslint-disable no-param-reassign */
-test('Object should be restructed', () => {
-  const takeInfo = jest.fn(({ id, name, description, icon }) => {
-    if (!description) {
-      description = 'Описание недоступно';
-    }
-    const obj = {
-      id,
-      name,
-      description,
-      icon,
-    };
-    const arr = [];
-    for (const prop in obj) {
-      // eslint-disable-next-line no-prototype-builtins
-      if (obj.hasOwnProperty(prop)) {
-        const objToSort = {
-          [prop]: obj[prop],
-        };
-        arr.push(objToSort);
-      }
-    }
-    return arr;
-  });
+import getSpecialAttack from '../app';
 
-  takeInfo({
-    id: 8,
-    name: 'Двойной выстрел',
-    icon: 'http://...',
-    description: 'Двойной выстрел наносит двойной урон',
-  });
+test('getSpecialAttack', () => {
+  const obj = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон',
+      },
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...',
+      },
+    ],
+  };
 
-  expect(takeInfo).toHaveReturnedWith([{ id: 8 },
-    { name: 'Двойной выстрел' },
-    { description: 'Двойной выстрел наносит двойной урон' },
-    { icon: 'http://...' },
-  ]);
+  const result = getSpecialAttack(obj);
+
+  const expected = [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      description: 'Описание недоступно',
+    },
+  ];
+
+  expect(result).toEqual(expected);
 });
-
-test('Description should be hidden', () => {
-  const takeInfo = jest.fn(({ id, name, description, icon }) => {
-    if (!description) {
-      description = 'Описание недоступно';
-    }
-    const obj = {
-      id,
-      name,
-      description,
-      icon,
-    };
-    const arr = [];
-    for (const prop in obj) {
-      // eslint-disable-next-line no-prototype-builtins
-      if (obj.hasOwnProperty(prop)) {
-        const objToSort = {
-          [prop]: obj[prop],
-        };
-        arr.push(objToSort);
-      }
-    }
-    return arr;
-  });
-
-  takeInfo({
-    id: 8,
-    name: 'Двойной выстрел',
-    icon: 'http://...',
-  });
-
-  expect(takeInfo).toHaveReturnedWith([{ id: 8 },
-    { name: 'Двойной выстрел' },
-    { description: 'Описание недоступно' },
-    { icon: 'http://...' },
-  ]);
-});
-
-// По этому заданию актуален вопрос с jest.fn, насколько я понимаю, это мока
-// выходит, что сначала я пишу функцию в файле, а потом пишу её еще раз в тесте,
-// мне кажется, что такой подход это много лишнего кода, существует ли способ без моки?
-// чтобы просто тест смотрел сразу на файл и видел, какое значение возвращается, я что в первом
-// задании, что во втором из-за этого воспринимаю моё решение как костыль... Без jest.fn при
-// импорте функции и обращении к ней получал постоянно ответ от jest, что нужно юзать mock, поэтому
-// реализация получилась такая, какая она есть.
-// Еще в этом задании проверка на покрытие тоже пустое, так что думаю это как раз из-за этого, если
-// функция не проверяется, то и покрывать нечего, так ли это и как это исправить?
-// Еще проблема в том, что порядок свойств почему то меняется и description c icon меняются местами,
-// является ли это ошибкой и если да, то как это исправить?
-// Еще линтер ругался на то, что я присваиваю в description значение, если оно undefined и на то,
-// что свойство объекта начинается не с новой строки, там, где это не влияет на визуальное удобное
-// восприятие я поправил, но, например в объявлении аргументов jest.fn это, на мой взгляд, только
-// мешает восприятию, поэтому эти 2 ошибки я добавил в игнор, важно ведь не только соблюдение
-// линтера, но и внешнее восприятие для разработчиков?
--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,14 @@
+/* eslint-disable one-var */
 /* eslint-disable no-param-reassign */
 /* eslint-disable object-curly-newline */
-const takeInfo = ({ id, name, description, icon }) => {
-  if (!description) {
-    description = 'Описание недоступно';
-  }
-  const obj = { id, name, description, icon };
-  const arr = [];
-  for (const prop in obj) {
-    // eslint-disable-next-line no-prototype-builtins
-    if (obj.hasOwnProperty(prop)) {
-      const objToSort = {
-        [prop]: obj[prop],
-      };
-      arr.push(objToSort);
-    }
-  }
-  return arr;
-};
+export default function getSpecialAttack(obj) {
+  const { special } = obj,
+    resultArr = [];
 
-takeInfo({
-  id: 8,
-  name: 'Двойной выстрел',
-  icon: 'http://...',
-  description: 'Двойной выстрел наносит двойной урон',
-});
+  for (const item of special) {
+    const { id, name, icon, description = 'Описание недоступно' } = item;
+    resultArr.push({ id, name, icon, description });
+  }
+
+  return resultArr;
+}
